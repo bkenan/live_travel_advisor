3faf4e9406d38e2e5cc80ba435305dd8f1393ccb
bkenan <kanan.bk@gmail.com>
2022-08-06 10:06:39 -0400

    add startrecording

--- static/main.js
+++ static/main.js
@@ -14,5 +14,37 @@ recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 
 
+function startRecording() {
+    console.log("recordButton clicked");
+
+    var constraints = { audio: true, video:false }
+
+    recordButton.disabled = true;
+    stopButton.disabled = false
+ 
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+        audioContext = new AudioContext();
+
+        /*  assign to gumStream for later use  */
+        gumStream = stream;
+
+        /* use the stream */
+        input = audioContext.createMediaStreamSource(stream);
+
+        rec = new Recorder(input,{numChannels:1})
+
+        //start the recording process
+        rec.record()
+
+        console.log("Recording started");
+
+    }).catch(function(err) {
+        //enable the record button if getUserMedia() fails
+        recordButton.disabled = false;
+        stopButton.disabled = true
+    });
+}
 
 
